5a4b765e2927c38cc17f138238436552b697de9b
Dorián Ádám <igigaberos@gmail.com>
2019-11-16 14:14:49 +0100

    Szorzótábla megoldása.

---
+++ Szorzotabla/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Szorzotabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AnswerButton4 = new System.Windows.Forms.Button();
+            this.AnswerButton2 = new System.Windows.Forms.Button();
+            this.AnswerButton1 = new System.Windows.Forms.Button();
+            this.AnswerButton3 = new System.Windows.Forms.Button();
+            this.MainLabel = new System.Windows.Forms.Label();
+            this.CorrectLabel = new System.Windows.Forms.Label();
+            this.WrongLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AnswerButton4
+            // 
+            this.AnswerButton4.Location = new System.Drawing.Point(287, 12);
+            this.AnswerButton4.Name = "AnswerButton4";
+            this.AnswerButton4.Size = new System.Drawing.Size(75, 23);
+            this.AnswerButton4.TabIndex = 0;
+            this.AnswerButton4.UseVisualStyleBackColor = true;
+            this.AnswerButton4.Click += new System.EventHandler(this.ClickAnswerButton);
+            // 
+            // AnswerButton2
+            // 
+            this.AnswerButton2.Location = new System.Drawing.Point(102, 12);
+            this.AnswerButton2.Name = "AnswerButton2";
+            this.AnswerButton2.Size = new System.Drawing.Size(75, 23);
+            this.AnswerButton2.TabIndex = 1;
+            this.AnswerButton2.UseVisualStyleBackColor = true;
+            this.AnswerButton2.Click += new System.EventHandler(this.ClickAnswerButton);
+            // 
+            // AnswerButton1
+            // 
+            this.AnswerButton1.Location = new System.Drawing.Point(12, 12);
+            this.AnswerButton1.Name = "AnswerButton1";
+            this.AnswerButton1.Size = new System.Drawing.Size(75, 23);
+            this.AnswerButton1.TabIndex = 2;
+            this.AnswerButton1.UseVisualStyleBackColor = true;
+            this.AnswerButton1.Click += new System.EventHandler(this.ClickAnswerButton);
+            // 
+            // AnswerButton3
+            // 
+            this.AnswerButton3.Location = new System.Drawing.Point(194, 12);
+            this.AnswerButton3.Name = "AnswerButton3";
+            this.AnswerButton3.Size = new System.Drawing.Size(75, 23);
+            this.AnswerButton3.TabIndex = 3;
+            this.AnswerButton3.UseVisualStyleBackColor = true;
+            this.AnswerButton3.Click += new System.EventHandler(this.ClickAnswerButton);
+            // 
+            // MainLabel
+            // 
+            this.MainLabel.AutoSize = true;
+            this.MainLabel.Location = new System.Drawing.Point(410, 22);
+            this.MainLabel.Name = "MainLabel";
+            this.MainLabel.Size = new System.Drawing.Size(36, 13);
+            this.MainLabel.TabIndex = 5;
+            this.MainLabel.Text = "Empty";
+            // 
+            // CorrectLabel
+            // 
+            this.CorrectLabel.AutoSize = true;
+            this.CorrectLabel.Location = new System.Drawing.Point(12, 60);
+            this.CorrectLabel.Name = "CorrectLabel";
+            this.CorrectLabel.Size = new System.Drawing.Size(99, 13);
+            this.CorrectLabel.TabIndex = 6;
+            this.CorrectLabel.Text = "Helyes valaszok:  0";
+            // 
+            // WrongLabel
+            // 
+            this.WrongLabel.AutoSize = true;
+            this.WrongLabel.Location = new System.Drawing.Point(12, 87);
+            this.WrongLabel.Name = "WrongLabel";
+            this.WrongLabel.Size = new System.Drawing.Size(108, 13);
+            this.WrongLabel.TabIndex = 7;
+            this.WrongLabel.Text = "Helytelen válaszok: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 769);
+            this.Controls.Add(this.WrongLabel);
+            this.Controls.Add(this.CorrectLabel);
+            this.Controls.Add(this.MainLabel);
+            this.Controls.Add(this.AnswerButton3);
+            this.Controls.Add(this.AnswerButton1);
+            this.Controls.Add(this.AnswerButton2);
+            this.Controls.Add(this.AnswerButton4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AnswerButton4;
+        private System.Windows.Forms.Button AnswerButton2;
+        private System.Windows.Forms.Button AnswerButton1;
+        private System.Windows.Forms.Button AnswerButton3;
+        private System.Windows.Forms.Label MainLabel;
+        private System.Windows.Forms.Label CorrectLabel;
+        private System.Windows.Forms.Label WrongLabel;
+    }
+}
+
---
+++ Szorzotabla/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szorzotabla
+{
+    public partial class Form1 : Form
+    {
+        private Random _randomGenerator;
+        private int _firstNumber, _secondNumber, _wrongAnswers, _correctAnswers;
+        private List<Button> _answerButtons;
+        private List<int> _answers;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _wrongAnswers = 0;
+            _correctAnswers = 0;
+            _answerButtons = new List<Button>() { AnswerButton1, AnswerButton2, AnswerButton3, AnswerButton4 };
+            _answers = new List<int>();
+
+            InitializeNumbers();
+            InitializeAnswerButtons();
+        }
+
+        private void InitializeAnswerButtons()
+        {
+            //helyes válasz véletlenszerű elhelyezése
+            int correctIndex = _randomGenerator.Next(0, 4);
+            _answerButtons[correctIndex].Text = (_firstNumber * _secondNumber).ToString();
+            _answers.Add(_firstNumber * _secondNumber);
+
+            //többi gombon egy egy véletlen szám elhelyezése
+            foreach (Button button in _answerButtons)
+            {
+                int randomGeneratedNumber = _randomGenerator.Next(1, 101);
+
+                //addig generálok számot amíg olyat nem generálok amit még nem generáltam
+                while (_answers.Contains(randomGeneratedNumber))
+                {
+                    randomGeneratedNumber = _randomGenerator.Next(1, 101);
+                }
+
+                _answers.Add(randomGeneratedNumber);
+
+                if (button.Text == string.Empty || button.Text == null)
+                {
+                    button.Text = randomGeneratedNumber.ToString();
+                }
+            }
+        }
+
+        private void ClickAnswerButton(object sender, EventArgs e)
+        {
+            if(((Button)sender).Text != (_firstNumber * _secondNumber).ToString())
+            {
+                ++_wrongAnswers;
+                WrongLabel.Text = $"Helytelen válaszok: {_wrongAnswers}";
+                ((Button)sender).Enabled = false;
+            }
+            else
+            {
+                ++_correctAnswers;
+                CorrectLabel.Text = $"Helyes válaszok: {_correctAnswers}";
+
+                InitializeNumbers();
+                ClearAnswerButtons();
+                InitializeAnswerButtons();
+                this.Invalidate();
+            }
+        }
+
+        private void ClearAnswerButtons()
+        {                          
+            foreach (var button in _answerButtons)
+            {
+                button.Enabled = true;
+                button.Text = string.Empty;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics grid = e.Graphics;
+
+            for (int i = 0; i < _firstNumber; ++i)
+            {
+                for (int j = 0; j < _secondNumber; ++j)
+                {
+                    Rectangle rectangle = new Rectangle(i * 40 + 5, j * 40 + 150, 38, 38);
+                    grid.FillRectangle(new SolidBrush(Color.Red), rectangle);
+                }
+            }
+        }
+
+        private void InitializeNumbers()
+        {
+            _randomGenerator = new Random();
+            _firstNumber = _randomGenerator.Next(1, 11);
+            Thread.Sleep(50);
+            _secondNumber = _randomGenerator.Next(1, 11);
+            //MainLabel.Text = "Mennyi" + _firstNumber + " x " + _secondNumber +" ?";
+            MainLabel.Text = $"Mennyi {_firstNumber} x {_secondNumber} ?";
+        }
+    }
+}
